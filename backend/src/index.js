47a6f8d909890249c4a0ed65a72b2852605a437c
Matheus <matheusbaron10@gmail.com>
2020-04-23 22:59:14 -0300

    Applying concepts to the first node app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
+
+app.use(express.json())
 
 /*
     Métodos HTTP:
@@ -10,30 +13,71 @@ const app = express()
     DELETE: Deletar uma informação no backend
 */
 
+/*
+    Tipos de parâmetros:
+    QUERY PARAMS: Filtros e paginação
+    ROUTE PARAMS: Identificar recursos na hora de atualizar ou deletar
+    REQUEST BODY: Conteúdo na hora de criar ou editar um recurso (JSON)
+*/
+
+const projects = []
+
 app.get('/', (request, response) => {
-    return response.json({ message: 'Bem vindo user!' })
+    return response.json({ message: 'Welcome user!' })
 })
 
 app.get('/projects', (request, response) => {
-    return response.json([
-        'project 1',
-        'project 2',
-    ])
+
+    const { title } = request.query
+
+    const results = title ?
+        projects.filter(project => project.title.includes(title)) :
+        projects
+
+    return response.json(results)
 })
 
-app.post('/projects/:id', (request, response) => {
-    return response.json([
-        'project 4',
-        'project 2',
-        'project 3',
-    ])
+app.post('/projects', (request, response) => {
+
+    const { title, owner } = request.body
+
+    const project = { id: uuid(), title, owner }
+    projects.push(project)
+
+    return response.json(project)
+})
+
+app.put('/projects/:id', (request, response) => {
+
+    const { id } = request.params
+    const { title, owner } = request.body
+
+    // O find procura dentro do item o ID que bate com o enviado via Route Params
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) return response.status(400).json({ error: 'Project not found' })
+
+    const project = {
+        id,
+        title,
+        owner
+    }
+
+    projects[projectIndex] = project
+
+    return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'project 1',
-        'project 2',
-    ])
+    const { id } = request.params
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) return response.status(400).json({ error: 'Project not found' })
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
 })
 
 app.listen(3333, () => {
