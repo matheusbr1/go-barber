ca5ee3438709fc91abbff17ad7df703ee705be63
Matheus <matheusbaron10@gmail.com>
2020-05-12 23:49:46 -0300

    Principles of SOLID applied

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,12 +1,12 @@
-import express, { json } from 'express'
+import express from 'express'
 
 import routes from './routes/index'
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 
-app.use(json)
 
 app.listen(3333, () => {
     console.log('Server started on port 3333')
