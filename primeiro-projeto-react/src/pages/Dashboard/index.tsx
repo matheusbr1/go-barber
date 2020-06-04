386717c2d57936908fd289ee0baa38a2dd4f9936
Matheus Baron <=>
2020-06-03 22:49:17 -0300

    HTML + CSS for repository page created

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import api from '../../services/api'
 import { Title, Form, Repositories, Error } from './styles'
@@ -80,14 +81,14 @@ const Dashboard: React.FC = () => {
                 {
                     repositories.map(repository =>
                         (
-                            <a key={repository.full_name} href={repository.html_url} target="_blank" >
+                            <Link key={repository.full_name} to={`/repository/${repository.full_name}`} >
                                 <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                                 <div>
                                     <strong>{repository.full_name}</strong>
                                     <p>{repository.description}</p>
                                 </div>
                                 <FiChevronRight size={20} />
-                            </a>
+                            </Link>
                         )
                     )
                 }
--- primeiro-projeto-react/src/pages/Repository/index.tsx
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,62 @@
 import React from 'react'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import { Header, RepositoryInfo, Issues } from './styles'
+import LogoImg from '../../assets/logo.svg'
+
+interface RepositoryParams {
+    repository: string
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>
+    const { params } = useRouteMatch<RepositoryParams>()
+
+    return (
+        <>
+            <Header>
+                <img src={LogoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                     Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/28275815?v=4" alt="be-the-hero" />
+                    <div>
+                        <strong>matheusbr/be-the-hero</strong>
+                        <p>This is an application built in the course of Rocketseat</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>1800</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to='fdsfsadfsa' >
+                    <div>
+                        <strong>fsdafsad</strong>
+                        <p>fdsafdsa</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    )
 }
 
 export default Repository
