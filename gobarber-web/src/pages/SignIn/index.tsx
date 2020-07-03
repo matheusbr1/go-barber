fd711e04983ab960e0bbb47855e02995841c7524
matheusbr1 <matheusbaron10@gmail.com>
2020-07-02 21:28:11 -0300

    SignIn Completed

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
+
 import { Container, Content, Background } from './styles'
 import logoImg from '../../assets/logo.svg'
+
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
