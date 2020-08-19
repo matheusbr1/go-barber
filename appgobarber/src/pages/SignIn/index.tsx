5723c76ead4b19516815191add0569b9fe4c4811
matheusbr1 <matheusbaron10@gmail.com>
2020-08-19 15:57:15 -0300

    Routes login and login and some adjusts for screen created

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,25 +1,63 @@
 import React from 'react'
-import { Image } from 'react-native'
-
-import { Container, Title } from './styles'
+import { 
+    Image,
+    View, 
+    ScrollView, 
+    KeyboardAvoidingView, 
+    Platform
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
-
 import logoImg from '../../assets/logo.png'
 
+import { 
+    Container,
+    Title, 
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText
+} from './styles'
+
 const SignIn: React.FC = () => {
+
+    const navigation = useNavigation()
+
     return (
-        <Container>
-            <Image source={logoImg} />
-            <Title>Faça seu logon</Title>
-
-
-            <Input name='email' icon='mail' placeholder='E-mail'/>
-            <Input name='password' icon='lock' placeholder='Senha' />
-
-            <Button onPress={() => console.log('ok')} >Entrar</Button>
-        </Container>
+        <>
+            <KeyboardAvoidingView 
+                    behavior={Platform.OS === 'ios' ? 'padding': undefined} 
+                    style={{ flex:1 }}
+                    enabled
+                >
+                <ScrollView 
+                    contentContainerStyle={{ flex:1 }}
+                    keyboardShouldPersistTaps='handled'
+                >
+                    <Container>
+                        <Image source={logoImg} />
+                        <View>
+                            <Title>Faça seu logon</Title>
+                        </View>
+                        <Input name='email' icon='mail' placeholder='E-mail'/>
+                        <Input name='password' icon='lock' placeholder='Senha' />
+                        <Button onPress={() => console.log('ok')} >Entrar</Button>
+                        <ForgotPassword onPress={() => console.log('Recover password')}>
+                            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <CreateAccountButton onPress={() => navigation.navigate('SignUp')} >
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>
+                    Criar uma conta
+                </CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
     )
 } 
 
--- appgobarber/src/pages/SignUp/styles.ts
+++ appgobarber/src/pages/SignUp/styles.ts
@@ -1,5 +1,39 @@
 import styled from 'styled-components/native'
+import { getBottomSpace } from 'react-native-iphone-x-helper'
+import { Platform } from 'react-native'
 
 export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    padding: 0 30px ${Platform.OS === 'android' ? 80 : 40 }px;
+`
 
+export const Title = styled.Text`
+    font-size: 24px;
+    color: 'rgb(244,237,232)';
+    font-family: 'RobotoSlab-Medium';
+    margin: 64px 0 24px; 
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: rgb(49, 46, 56);
+    border-top-width: 1px;
+    border-color: rgb(35, 33, 41);
+    padding:16px 0${16 + getBottomSpace()}px;
+
+    justify-content:center;
+    align-items: center;
+    flex-direction: row;
+`
+
+export const BackToSignInText = styled.Text`
+    color:rgb(255, 255, 255);
+    font-size:18px;
+    font-family: 'RobotoSlab-Regular';
+    margin-left:16px
 `
