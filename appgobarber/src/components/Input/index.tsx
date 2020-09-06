c8e86cfcbeeb54c2bb9b6ac118c6801ea25ff88e
matheusbr1 <matheusbaron10@gmail.com>
2020-09-06 13:16:45 -0300

    Focused and icon color added to input

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react'
+import React, { useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback } from 'react'
 import { Container, TextInput, Icon } from './styles'
 import { useField } from '@unform/core'
 
@@ -23,6 +23,18 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = ({ name, icon,
     const { registerField, defaultValue = '', fieldName, error }  = useField(name)
     const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
 
+    const [isFocused, setIsFocused] = useState(false)
+    const [isFilled, setIsFilled] = useState(false)
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true)
+    },[])
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false)
+        setIsFilled(!!inputValueRef.current.value)
+    },[])
+
     useImperativeHandle(ref, () => ({
         focus() {
             inputElementRef.current.focus()
@@ -46,12 +58,14 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = ({ name, icon,
     },[fieldName, registerField])
 
     return (
-        <Container>
-            <Icon name={icon} size={20} color='#666360' />
+        <Container isFocused={isFocused} >
+            <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
             <TextInput 
                 ref = {inputElementRef}
                 keyboardAppearance='dark'
                 placeholderTextColor='#666360' 
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
                 onChangeText={value => {
                     inputValueRef.current.value = value
                 }}
