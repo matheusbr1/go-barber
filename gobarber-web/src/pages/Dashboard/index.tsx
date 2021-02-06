052ba2188a235089cd5b1ba2674cbdd4c5828b32
Matheus <=>
2021-02-06 13:26:39 -0300

    List of appointments created

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { Container, Header, HeaderContent, Profile } from './styles'
+import React, { useState } from 'react'
+import { 
+  Container, 
+  Header, 
+  HeaderContent, 
+  Profile, 
+  Content, 
+  Schedule, 
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles'
 
 import logoImg from '../../assets/logo.svg'
-import { FiPower } from 'react-icons/fi'
+import { FiClock, FiPower } from 'react-icons/fi'
 import { useAuth } from '../../hooks/auth'
 
 const Dashboard: React.FC = () => {
+
+  const [selectedDate, setSelectedDate] = useState(new Date())
 
   const { signOut, user } = useAuth()
 
   return (
     <Container>
       <Header>
-        <HeaderContent>
+        <HeaderContent>z
           <img src={logoImg} alt="GoBarber"/>
           <Profile>
             <img src="https://avatars.githubusercontent.com/u/28275815?s=460&u=07b68060e0c143581888029bef65ac1496406262&v=4" alt={user.name}/>
@@ -27,6 +40,94 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+
+        <Schedule>
+
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-Feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src='https://avatars.githubusercontent.com/u/28275815?s=460&u=07b68060e0c143581888029bef65ac1496406262&v=4' alt="Matheus Baron"/>
+
+              <strong>Matheus Baron</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src='https://avatars.githubusercontent.com/u/28275815?s=460&u=07b68060e0c143581888029bef65ac1496406262&v=4' alt="Matheus Baron"/>
+
+                <strong>Matheus Baron</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src='https://avatars.githubusercontent.com/u/28275815?s=460&u=07b68060e0c143581888029bef65ac1496406262&v=4' alt="Matheus Baron"/>
+
+                <strong>Matheus Baron</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src='https://avatars.githubusercontent.com/u/28275815?s=460&u=07b68060e0c143581888029bef65ac1496406262&v=4' alt="Matheus Baron"/>
+
+                <strong>Matheus Baron</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src='https://avatars.githubusercontent.com/u/28275815?s=460&u=07b68060e0c143581888029bef65ac1496406262&v=4' alt="Matheus Baron"/>
+
+                <strong>Matheus Baron</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+        </Schedule>
+        <Calendar />
+
+      </Content>
+
     </Container>
   ) 
 } 
