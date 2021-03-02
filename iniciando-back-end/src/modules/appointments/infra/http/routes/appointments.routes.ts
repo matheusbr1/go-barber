40e636e0c57262e30b8a832260e85bcec271a445
matheusbr1 <matheusbaron10@gmail.com>
2021-03-01 22:02:40 -0300

    Feat: ListProviderAppointments service created

--- iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,19 +1,20 @@
 import { Router } from 'express'
 
-import AppointmentsController from '../controllers/AppointmentController'
-
 // Middleware of authentication
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
-// SoC -> Separation of concerns
+import AppointmentsController from '../controllers/AppointmentController'
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController'
 
 const appointmentsRouter = Router()
-const appointmentsControllers = new AppointmentsController()
+const appointmentsController = new AppointmentsController()
+const providerAppointmentsController = new ProviderAppointmentsController()
 
 // Apply middleware on all routes
 appointmentsRouter.use(ensureAuthenticated)
 
 // Routes
-appointmentsRouter.post('/', appointmentsControllers.create)
+appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.get('/me', providerAppointmentsController.index)
 
 export default appointmentsRouter
