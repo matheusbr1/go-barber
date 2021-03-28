338375fb410ce4f359540ae0b5260d28dea2714a
matheusbr1 <matheusbaron10@gmail.com>
2021-03-28 18:45:22 -0300

    Fix: adjusts in the splash screen

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,20 +1,29 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, StatusBar } from 'react-native'
 import AppProvider from './hooks'
 
+import SplashScreen from 'react-native-splash-screen'
+
 import Routes from './routes'
 
-const App: React.FC = () => (
-    <NavigationContainer>
-        <StatusBar barStyle='light-content' backgroundColor='#312e38' />
-        <AppProvider>
-            <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-                <Routes />
-            </View>
-        </AppProvider>
-    </NavigationContainer>
-)
+const App: React.FC = () => {
+
+    useEffect(() => {
+        SplashScreen.hide()
+    }, [])
+
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle='light-content' backgroundColor='#312e38' />
+            <AppProvider>
+                <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+                    <Routes />
+                </View>
+            </AppProvider>
+        </NavigationContainer>
+    )
+} 
 
 export default App
